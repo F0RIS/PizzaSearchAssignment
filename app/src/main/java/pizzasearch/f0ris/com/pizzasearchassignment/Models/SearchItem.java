ca249b2acdf5a30df658806a8745949cbccd5436
F0RIS <F0RISW@gmail.com>
2016-03-06 22:39:16 +0200

    deleted bitmap field from SearchItem

--- app/src/main/java/pizzasearch/f0ris/com/pizzasearchassignment/Models/SearchItem.java
+++ app/src/main/java/pizzasearch/f0ris/com/pizzasearchassignment/Models/SearchItem.java
@@ -1,6 +1,7 @@
 package pizzasearch.f0ris.com.pizzasearchassignment.Models;
 
 import android.graphics.Bitmap;
+import android.support.annotation.NonNull;
 
 /**
  * Created by F0RIS on 05.03.2016.
@@ -9,14 +10,12 @@ public class SearchItem implements Comparable<SearchItem>{
 
     public String name;
     public String photo_url;
-    public Bitmap photo;
 
     public int distance;
     public float rating;
 
-
     @Override
-    public int compareTo(SearchItem another) {
+    public int compareTo(@NonNull SearchItem another) {
         return this.distance - another.distance;
     }
 }
